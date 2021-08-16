de0debf73cf96fa05c2460155b689705309c5d29
tayzarmein <tayzarmein@gmail.com>
2021-08-16 21:44:43 +0900

    setup apollo

---
+++ api-server/src/datasources/productApi.js
@@ -0,0 +1,7 @@
+const {MongoDataSource} = require('apollo-datasource-mongodb')
+
+class Products extends MongoDataSource {
+
+}
+
+module.exports = Products
--- api-server/src/index.js
+++ api-server/src/index.js
@@ -1,17 +1,37 @@
-const {ApolloServer} = require('apollo-server');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers')
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const Products = require("./datasources/productApi");
 
-const server = new ApolloServer({
-    typeDefs, 
-    resolvers
-});
+require("dotenv").config();
 
+const { MongoClient } = require("mongodb");
 
-server.listen().then(() => {
+async function getDb() {
+  const client = new MongoClient(
+    `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}`
+  );
+  await client.connect();
+  const db = client.db(process.env.DB_NAME);
+
+  console.log("db connected");
+  return db;
+}
+
+getDb().then((db) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+      products: new Products(db.collection('products'))
+    })
+  });
+
+  server.listen().then(() => {
     console.log(`
       🚀  Server is running!
       🔉  Listening on port 4000
       📭  Query at https://studio.apollographql.com/dev
     `);
   });
+});
