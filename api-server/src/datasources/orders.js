cb0c1fb1669d3ea98c21247c5267dd04ecb4b133
thawzin-11 <skydark.thawzintun@gmail.com>
2021-10-04 14:48:36 +0630

    graphql mutation for orders

---
+++ api-server/src/datasources/orders.js
@@ -0,0 +1,34 @@
+const {MongoDataSource} = require('apollo-datasource-mongodb');
+
+class Orders extends MongoDataSource {
+
+    async getAllOrders () {
+        const orders = await this.collection.find().toArray();
+        // console.log(orders);
+        const listOfOrders = orders.map((order) => {
+          order.id = order._id
+          return order;
+        })
+        return listOfOrders;
+    }
+
+    getProductsFromOrder (order) {
+        // console.log('listofProducts=', order.products)
+        const listOfProducts = order.products;
+        return listOfProducts;
+    }
+
+    getUserFromOrder (order) {
+        const orderUser = order.user;
+        return {
+            name: orderUser.name,
+            phNumber: orderUser.phNumber
+        }
+    }
+    async addInputOrder (inputOrder) {
+        const dummy = await this.collection.insertOne(inputOrder.order);
+        return dummy
+    }
+}
+
+module.exports = Orders;
--- api-server/src/resolvers.js
+++ api-server/src/resolvers.js
@@ -13,11 +13,21 @@ const resolvers = {
             })
             return categoriesObjects;
         },
-        users: (parent, args, context, info) => {
-            // console.log(context.dataSources.products.getAllUsers())
-            const aa = context.dataSources.users.getAllUsers();
-            // console.log(aa)
-            return aa;
+        users: async (parent, args, context, info) => {
+            const users = await context.dataSources.users.getAllUsers();
+            const usersObjects = users.map((user) => {
+                return {
+                    id: user._id,
+                    name: user.name,
+                    phNumber: user.phNumber
+                }
+            })
+            return usersObjects;
+        },
+        orders: async (parent, args, context, info) => {
+            const orders = await context.dataSources.orders.getAllOrders();
+            // console.log('orders', orders);
+            return orders;
         }
     },
     Category: {
@@ -27,19 +37,47 @@ const resolvers = {
             return products;
         }
     },
+
+    Order: {
+        products: (parent, args, context, info) => {
+            //    console.log('parent=', parent)
+            const products = context.dataSources.orders.getProductsFromOrder(parent);
+            return products;
+        }
+    },
+
+    Order: {
+        user: (parent, args, context, info) => {
+            // console.log('parent=', parent)
+            const user = context.dataSources.orders.getUserFromOrder(parent);
+            return user;
+        }
+    },
+
     Mutation: {
         addUser: async (parent, args, context, info) => {
-            await context.dataSources.users.addSingleUser(args);
+            const addedUser = await context.dataSources.users.addSingleUser(args);
             return {
-                code: 200,
-                success: true,
-                message: "User Successfully added",
+                // code: 200,
+                // success: true,
                 user: {
+                    id: addedUser.insertedId,
                     name: args.name,
-                    phNumber: args.phNumber 
-                }
+                    phNumber: args.phNumber
+                },
+                message: "User Successfully Added",
+                success: addedUser.acknowledged,
+
             }
-            
+        },
+        addOrder: async (parent, args, context, info) => {
+            const inputtedOrder = await context.dataSources.orders.addInputOrder(args);
+            return {
+                id: inputtedOrder.insertedId,
+                success: inputtedOrder.acknowledged,
+                message: "Order Successfully Added"
+            }
+
         }
     }
 }
